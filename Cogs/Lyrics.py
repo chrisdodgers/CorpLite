531b524396ae5c637f330c4e502650ade4bf2f70
EggToaster <toshikawa907@gmail.com>
2025-02-09 20:49:12 +0900

    Added Lyrics module

---
+++ Cogs/Lyrics.py
@@ -0,0 +1,65 @@
+import asyncio, discord
+from   discord.ext import commands
+from   Cogs import Utils, Message, DisplayName, PickList
+from   lyricsgenius import Genius
+
+def setup(bot):
+    settings = bot.get_cog("Settings")
+    if not bot.settings_dict.get("lyrics"):
+        if not bot.settings_dict.get("suppress_disabled_warnings"):     
+            print("\n!! Lyrics Cog has been disabled !!")
+            print("* Lyrics API key is missing ('lyrics' in settings_dict.json)")
+            print("* You can get a free Genius API key by signing up at:")
+            print("   https://genius.com/api-clients")
+            return
+    bot.add_cog(Lyrics(bot, settings))
+
+class Lyrics(commands.Cog):
+
+    def __init__(self, bot, settings):
+        self.bot = bot
+        self.settings = settings
+        global Utils, DisplayName
+        Utils = self.bot.get_cog("Utils")
+        DisplayName = self.bot.get_cog("DisplayName")
+
+    @commands.command(aliases=['lyric'])
+    async def lyrics(self, ctx, *, query : str):
+        """Get the lyrics for a song"""
+        genius = Genius(self.bot.settings_dict.get("lyrics"))
+        message = await ctx.send("Searching for lyrics...")
+        song = None
+        try:
+            song = genius.search_song(query)
+        except Exception as e:
+            await message.edit(content=f"Error retrieving lyrics")
+            print(e)
+            return
+        if song != None:
+            title = song.title
+            artist = song.artist
+            url = song.url
+            lyrics = "\n".join(song.lyrics.split("\n")[1:]) # Remove the first line, as it contains information in a weird format
+            # Remove "You might also likeEmbed" if it exists
+            if "You might also likeEmbed" in lyrics:
+                lyrics = lyrics.split("You might also likeEmbed")[0].strip()
+
+            if len(lyrics) < 1500:
+                await Message.EmbedText(
+                    title="**{}** by **{}**".format(title, artist),
+                    description=lyrics,
+                    color=ctx.author,
+                    footer="Powered by Genius",
+                    url=url
+                ).send(ctx, message)
+            else:
+                return await PickList.PagePicker(
+                    title="**{}** by **{}**".format(title, artist),
+                    ctx=ctx,
+                    description=lyrics,
+                    timeout=180,
+                    url=url,
+                    footer="Powered by Genius"
+                ).pick()
+        else:
+            message.edit(content="No results found for that query.")
